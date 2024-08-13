436d89bf155d38edcd719e0727d9b674996709bd
JTek2022 <111205487+JTek2022@users.noreply.github.com>
2024-08-13 14:28:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -53,10 +53,10 @@ def send_email(location):
         server.send_message(msg)
         st.success("📨 Email sent successfully!")
         st.balloons()
-        st.info("Please return to the form, enter your result and continue to the next section.")
+        st.info("💡Please return to the form, enter your result and continue to the next section.")
     except Exception as e:
         st.error(f"⚠️ An error occurred while sending the email: {e}")
-        st.info("Please return to the form, enter your result and continue to the next section.")
+        st.info("💡Please return to the form, enter your result and continue to the next section.")
     finally:
         server.quit()
 
